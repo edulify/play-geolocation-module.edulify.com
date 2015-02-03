da422afca9ab8d7dee2fb5063234fc2cba74b9e1
Megazord <contact@edulify.com>
2015-02-03 02:59:53 -0200

    Geolocation started as a play plugin

---
+++ project-code/app/com/edulify/modules/geolocation/GeolocationPlugin.java
@@ -0,0 +1,33 @@
+package com.edulify.modules.geolocation;
+
+public class GeolocationPlugin extends play.Plugin {
+  
+  @Override
+  public void onStart() {
+    super.onStart();
+    try {
+      Class<?> providerClass = Class.forName(Config.getStringOr("geolocation.provider", ""));
+      play.Logger.info("Starting GeolocationPlugin with provider " + providerClass);
+      GeolocationProvider provider = (GeolocationProvider) providerClass.newInstance();
+      AsyncGeolocationService.initialize(provider);
+      GeolocationService.initialize(provider);
+    } catch (ClassNotFoundException ex) {
+      throw new RuntimeException("Could not initialize GeolocationPlugin. Check if class configured in geolocation.provider exists", ex);
+    } catch (InstantiationException ex) {
+      throw new RuntimeException("Could not initialize GeolocationPlugin. Check if class configured in geolocation.provider has a default constructor", ex);
+    } catch (IllegalAccessException ex) {
+      throw new RuntimeException("Could not initialize GeolocationPlugin. Check if class configured in geolocation.provider has a public constructor", ex);
+    }
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    play.Logger.info("Stopping GeolocationPlugin...");
+  }
+
+  @Override
+  public boolean enabled() {
+    return Config.getBooleanOr("geolocation.enabled", true);
+  }
+}
