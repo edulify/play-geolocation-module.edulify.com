0e302eac5219bd3711f4b203704aa801bec66b4b
Megazord <contact@edulify.com>
2015-02-03 21:54:19 -0200

    Sample application with async actions

--- sample/java/app/controllers/Application.java
+++ sample/java/app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.libs.F;
 
 import views.html.*;
 
@@ -9,51 +10,95 @@ import utils.GeolocationHelper;
 
 import com.edulify.modules.geolocation.Geolocation;
 import com.edulify.modules.geolocation.GeolocationService;
+import com.edulify.modules.geolocation.AsyncGeolocationService;
 
 public class Application extends Controller {
 
-    public static Result index() {
-      return ok(index.render("Your new application is ready."));
+  public static Result index() {
+    return ok(index.render("Your new application is ready."));
+  }
+
+  public static Result syncCountry(String addr) {
+    if (GeolocationHelper.isReserved(addr)) {
+      return ok(index.render("This ip is reserved."));
     }
 
-    public static Result syncCountry(String addr) {
-      if (GeolocationHelper.isReserved(addr)) {
-        return ok(index.render("This ip is reserved."));
-      }
+    Geolocation geolocation = GeolocationService.getGeolocation(addr);
+    String country = geolocation.getCountryName();
+    String message = country != null ? String.format("This ip comes from %s", country) :
+        "Sorry, we couldn't connect to the webservice";
+    return ok(index.render(message));
+  }
 
-      Geolocation geolocation = GeolocationService.getGeolocation(addr);
-      String country = geolocation.getCountryName();
-      String message = country != null ? String.format("This ip comes from %s", country) :
-                                           "Sorry, we couldn't connect to the webservice";
-      return ok(index.render(message));
+  public static Result syncCountryCode(String addr) {
+    if (GeolocationHelper.isReserved(addr)) {
+      return ok(index.render("This ip is reserved."));
     }
 
-    public static Result syncCountryCode(String addr) {
-      if (GeolocationHelper.isReserved(addr)) {
-        return ok(index.render("This ip is reserved."));
-      }
+    Geolocation geolocation = GeolocationService.getGeolocation(addr);
+    String countryCode = geolocation.getCountryCode();
+    String message = countryCode != null ? String.format("This ip comes from %s", countryCode) :
+        "Sorry, we couldn't connect to the webservice";
+    return ok(index.render(message));
+  }
 
-      Geolocation geolocation = GeolocationService.getGeolocation(addr);
-      String countryCode = geolocation.getCountryCode();
-      String message     = countryCode != null ? String.format("This ip comes from %s", countryCode) :
-            "Sorry, we couldn't connect to the webservice";
-      return ok(index.render(message));
+  public static Result syncGeolocation(String addr) {
+    Geolocation geolocation = GeolocationService.getGeolocation(addr);
+    if (geolocation == null) {
+      return ok(index.render("Sorry, we couldn't connect to the webservice"));
     }
+    return ok(geoData.render(addr,
+        geolocation.getIp(),
+        geolocation.getCountryCode(),
+        geolocation.getCountryName(),
+        geolocation.getRegionCode(),
+        geolocation.getRegionName(),
+        geolocation.getCity(),
+        geolocation.getLatitude(),
+        geolocation.getLongitude()));
+  }
 
-    public static Result ipGeolocation(String addr) {
-      Geolocation geolocation = GeolocationService.getGeolocation(addr);
-      if (geolocation == null) {
-        return ok(index.render("Sorry, we couldn't connect to the webservice"));
-      }
-      return ok(geoData.render(addr,
-          geolocation.getIp(),
-          geolocation.getCountryCode(),
-          geolocation.getCountryName(),
-          geolocation.getRegionCode(),
-          geolocation.getRegionName(),
-          geolocation.getCity(),
-          geolocation.getLatitude(),
-          geolocation.getLongitude()));
+  public static F.Promise<Result> asyncCountry(final String addr) {
+    return AsyncGeolocationService.getGeolocation(addr)
+        .map(new F.Function<Geolocation, Result>() {
+          public Result apply(Geolocation geolocation) {
+            String country = geolocation.getCountryName();
+            String message = country != null ? String.format("This ip comes from %s", country) :
+                "Sorry, we couldn't connect to the webservice";
+            return ok(index.render(message));
+          }
+        });
+  }
 
-    }
+  public static F.Promise<Result> asyncCountryCode(final String addr) {
+    return AsyncGeolocationService.getGeolocation(addr)
+        .map(new F.Function<Geolocation, Result>() {
+          public Result apply(Geolocation geolocation) {
+            String countryCode = geolocation.getCountryCode();
+            String message = countryCode != null ? String.format("This ip comes from %s", countryCode) :
+                "Sorry, we couldn't connect to the webservice";
+            return ok(index.render(message));
+          }
+        });
+  }
+
+  public static F.Promise<Result> asyncGeolocation(final String addr) {
+    return AsyncGeolocationService.getGeolocation(addr)
+        .map(new F.Function<Geolocation, Result>() {
+          public Result apply(Geolocation geolocation) {
+            String countryCode = geolocation.getCountryCode();
+            String message = countryCode != null ? String.format("This ip comes from %s", countryCode) :
+                "Sorry, we couldn't connect to the webservice";
+            return ok(geoData.render(addr,
+                geolocation.getIp(),
+                geolocation.getCountryCode(),
+                geolocation.getCountryName(),
+                geolocation.getRegionCode(),
+                geolocation.getRegionName(),
+                geolocation.getCity(),
+                geolocation.getLatitude(),
+                geolocation.getLongitude()));
+          }
+        });
+  }
 }
